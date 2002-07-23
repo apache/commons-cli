991e627d88b40d246f0fe29367994cbec0c1d3ff
Henri Yandell <bayard@apache.org>
2002-07-23 03:05:42 +0000

    Moved back to using Commons.lang.Numbers and not NumberUtils. CLI depends on the Maven repository and this will not be receiving the NumberUtils class for a short while.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/trunk@129786 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/cli/TypeHandler.java
+++ src/java/org/apache/commons/cli/TypeHandler.java
@@ -66,7 +66,7 @@ import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.Date;
 
-import org.apache.commons.lang.NumberUtils;
+import org.apache.commons.lang.Numbers;
 
 /**
   * This is a temporary implementation. TypeHandler will handle the 
@@ -175,7 +175,7 @@ public class TypeHandler {
         // Needs to be able to create
         try {
             // do searching for decimal point etc, but atm just make an Integer
-            return NumberUtils.createNumber(str);
+            return Numbers.createNumber(str);
         } catch (NumberFormatException nfe) {
             System.err.println(nfe.getMessage());
             return null;
