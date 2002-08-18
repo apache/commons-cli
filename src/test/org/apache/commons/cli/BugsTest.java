469e71799a438ccb2d0925e50d4bb9dce37cdba2
John Keyes <jkeyes@apache.org>
2002-08-18 15:52:23 +0000

    bug no 11458: added test
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/trunk@129802 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/BugsTest.java
+++ src/test/org/apache/commons/cli/BugsTest.java
@@ -5,7 +5,7 @@
  * version 1.1, a copy of which has been included with this distribution in
  * the LICENSE file.
  * 
- * $Id: BugsTest.java,v 1.2 2002/08/15 22:05:19 jkeyes Exp $
+ * $Id: BugsTest.java,v 1.3 2002/08/18 15:52:23 jkeyes Exp $
  */
 
 package org.apache.commons.cli;
@@ -35,6 +35,58 @@ public class BugsTest extends TestCase
 
     public void tearDown()
     {
+    }
+
+    public void test11458()
+    {
+        Options options = new Options();
+        options.addOption( OptionBuilder.withValueSeparator( '=' )
+                           .hasArgs()
+                           .create( 'D' ) );
+        options.addOption( OptionBuilder.withValueSeparator( ':' )
+                           .hasArgs()
+                           .create( 'p' ) );
+        String[] args = new String[] { "-DJAVA_HOME=/opt/java" ,
+        "-pfile1:file2:file3" };
+
+        CommandLineParser parser = CommandLineParserFactory.newParser();
+
+        try {
+            CommandLine cmd = parser.parse( options, args );
+
+            String[] values = cmd.getOptionValues( 'D' );
+
+            assertEquals( values[0], "JAVA_HOME" );
+            assertEquals( values[1], "/opt/java" );
+
+            values = cmd.getOptionValues( 'p' );
+
+            assertEquals( values[0], "file1" );
+            assertEquals( values[1], "file2" );
+            assertEquals( values[2], "file3" );
+
+            java.util.Iterator iter = cmd.iterator();
+            while( iter.hasNext() ) {
+                Option opt = (Option)iter.next();
+                switch( opt.getId() ) {
+                    case 'D':
+                        assertEquals( opt.getValue( 0 ), "JAVA_HOME" );
+                        assertEquals( opt.getValue( 1 ), "/opt/java" );
+                        break;
+                    case 'p':
+                        assertEquals( opt.getValue( 0 ), "file1" );
+                        assertEquals( opt.getValue( 1 ), "file2" );
+                        assertEquals( opt.getValue( 2 ), "file3" );
+                        break;
+                    default:
+                        fail( "-D option not found" );
+                }
+            }
+        }
+        catch( ParseException exp ) {
+            fail( "Unexpected Exception:\nMessage:" + exp.getMessage() 
+                  + "Type: " + exp.getClass().getName() );
+        }
     }
 
     public void test11680()
