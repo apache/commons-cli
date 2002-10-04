f577183054dd835d31a3f40026fe63831c140791
James Strachan <jstrachan@apache.org>
2002-10-04 11:06:58 +0000

    Minor patch so that the unit test cases work on Windows. The main change is that end of line characters can be different on different platforms. So made the test less whitespace sensitive.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/trunk@129818 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/TestHelpFormatter.java
+++ src/test/org/apache/commons/cli/TestHelpFormatter.java
@@ -150,22 +150,22 @@ public class TestHelpFormatter extends TestCase
    {
        HelpFormatter hf = new HelpFormatter();
        Options options = null;
-       String expected = "usage: app [-a]\n";
+       String expected = "usage: app [-a]";
        ByteArrayOutputStream out = new ByteArrayOutputStream( );
        PrintWriter pw = new PrintWriter( out );
 
        options = new Options().addOption("a", false, "aaaa aaaa aaaa aaaa aaaa");
        hf.printUsage( pw, 60, "app", options );
        pw.flush();
-       assertEquals("simple auto usage", expected, out.toString());
+       assertEquals("simple auto usage", expected, out.toString().trim());
        out.reset();
 
-       expected = "usage: app [-b] [-a]\n";
+       expected = "usage: app [-b] [-a]";
        options = new Options().addOption("a", false, "aaaa aaaa aaaa aaaa aaaa")
        .addOption("b", false, "bbb" );
        hf.printUsage( pw, 60, "app", options );
        pw.flush();
-       assertEquals("simple auto usage", expected, out.toString());
+       assertEquals("simple auto usage", expected, out.toString().trim());
        out.reset();
    }
 }
