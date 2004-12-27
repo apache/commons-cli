fe5fd7e9cb8482c8ae5ae08e3679390781f425aa
Robert James Oxspring <roxspring@apache.org>
2004-12-27 15:25:53 +0000

    Added test to demonstrate fix of 19383
    BR: 19383
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/trunk@130114 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/BugsTest.java
+++ src/test/org/apache/commons/cli/BugsTest.java
@@ -514,5 +514,23 @@ public class BugsTest extends TestCase
                 "Footer"+SEP
                 ,out.toString());
     }
+    
+    public void test19383() {
+        Options options = new Options();
+        options.addOption(new Option("a","aaa",false,"aaaaaaa"));
+        options.addOption(new Option(null,"bbb",false,"bbbbbbb"));
+        options.addOption(new Option("c",null,false,"ccccccc"));
+        
+        HelpFormatter formatter = new HelpFormatter();
+        String SEP = System.getProperty("line.separator");
+        StringWriter out = new StringWriter();
+        formatter.printHelp(new PrintWriter(out),80, "foobar", "", options, 2, 2, "", true);
+        assertEquals(
+                "usage: foobar [-a] [-c] [--bbb]"+SEP+
+                "  -a,--aaa  aaaaaaa"+SEP+
+                "     --bbb  bbbbbbb"+SEP+
+                "  -c        ccccccc"+SEP
+                ,out.toString());
+    }
 
 }
