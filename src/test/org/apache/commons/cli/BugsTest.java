798f9e5c5c508d2edf86300b8fc00687d4a4f914
Robert James Oxspring <roxspring@apache.org>
2004-12-27 13:33:48 +0000

    Added test to demonstrate the 27635 has been fixed
    PR: 27635
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/trunk@130111 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/BugsTest.java
+++ src/test/org/apache/commons/cli/BugsTest.java
@@ -17,6 +17,7 @@ package org.apache.commons.cli;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import junit.framework.Test;
@@ -409,6 +410,74 @@ public class BugsTest extends TestCase
         options.addOption( m );
         CommandLine line = parser.parse( options, args );
         assertEquals( "Two Words", line.getOptionValue( "m" ) );
+    }
+
+    public void test27635() {
+        Option help = new Option("h", "help", false, "print this message");
+        Option version = new Option("v", "version", false, "print version information");
+        Option newRun = new Option("n", "new", false, "Create NLT cache entries only for new items");
+        Option trackerRun = new Option("t", "tracker", false, "Create NLT cache entries only for tracker items");
+        
+        Option timeLimit = OptionBuilder.withLongOpt("limit")
+                                        .hasArg()
+                                        .withValueSeparator()
+                                        .withDescription("Set time limit for execution, in mintues")
+                                        .create("l");
+        
+        Option age = OptionBuilder.withLongOpt("age")
+                                        .hasArg()
+                                        .withValueSeparator()
+                                        .withDescription("Age (in days) of cache item before being recomputed")
+                                        .create("a");
+        
+        Option server = OptionBuilder.withLongOpt("server")
+                                        .hasArg()
+                                        .withValueSeparator()
+                                        .withDescription("The NLT server address")
+                                        .create("s");
+        
+        Option numResults = OptionBuilder.withLongOpt("results")
+                                        .hasArg()
+                                        .withValueSeparator()
+                                        .withDescription("Number of results per item")
+                                        .create("r");
+        
+        Option configFile = OptionBuilder.withLongOpt("config")
+                                        .hasArg()
+                                        .withValueSeparator()
+                                        .withDescription("Use the specified configuration file")
+                                        .create();
+        
+        Options mOptions = new Options();
+        mOptions.addOption(help);
+        mOptions.addOption(version);
+        mOptions.addOption(newRun);
+        mOptions.addOption(trackerRun);
+        mOptions.addOption(timeLimit);
+        mOptions.addOption(age);
+        mOptions.addOption(server);
+        mOptions.addOption(numResults);
+        mOptions.addOption(configFile);
+        
+        HelpFormatter formatter = new HelpFormatter();
+        final String EOL = System.getProperty("line.separator");
+        StringWriter out = new StringWriter();
+        formatter.printHelp(new PrintWriter(out),80,"commandline","header",mOptions,2,2,"footer",true);
+        assertEquals(
+                "usage: commandline [--config <arg>] [-r <arg>] [-a <arg>] [-h] [-t] [-n] [-l"+EOL+
+                "       <arg>] [-s <arg>] [-v]"+EOL+
+                "header"+EOL+
+                "  -a,--age <arg>      Age (in days) of cache item before being recomputed"+EOL+
+                "     --config <arg>   Use the specified configuration file"+EOL+
+                "  -h,--help           print this message"+EOL+
+                "  -l,--limit <arg>    Set time limit for execution, in mintues"+EOL+
+                "  -n,--new            Create NLT cache entries only for new items"+EOL+
+                "  -r,--results <arg>  Number of results per item"+EOL+
+                "  -s,--server <arg>   The NLT server address"+EOL+
+                "  -t,--tracker        Create NLT cache entries only for tracker items"+EOL+
+                "  -v,--version        print version information"+EOL+
+                "footer"+EOL
+                ,out.toString());
     }
 
 }
