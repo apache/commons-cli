bfe6bd8634895645aa71d6a6dc668545297d7413
Henri Yandell <bayard@apache.org>
2007-06-29 07:10:22 +0000

    Applying second patch from CLI-135 - fixing the mistake of having an Object and not a String as the parameter to addValue.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/cli/branches/cli-1.0.x@551816 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/cli/Option.java
+++ src/java/org/apache/commons/cli/Option.java
@@ -649,7 +649,7 @@ public class Option implements Cloneable {
         this.values.clear();
     }
 
-    public boolean addValue(Object object) {
+    public boolean addValue(String value) {
         throw new UnsupportedOperationException(
            "The addValue method is not intended for client use. " + 
            "Subclasses should use the addValueForProcessing method instead. "
