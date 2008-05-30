8214cec49675d881c49a22172a20edee09add6f7
Emmanuel Bourg <ebourg@apache.org>
2008-05-30 11:13:13 +0000

    Added tests for the accessors of HelpFormatter
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/branches/cli-1.x@661649 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/HelpFormatterTest.java
+++ src/test/org/apache/commons/cli/HelpFormatterTest.java
@@ -308,4 +308,33 @@ public class HelpFormatterTest extends TestCase
         assertEquals("  foo", formatter.rtrim("  foo  "));
     }
 
+    public void testAccessors()
+    {
+        HelpFormatter formatter = new HelpFormatter();
+
+        formatter.setArgName("argname");
+        assertEquals("arg name", "argname", formatter.getArgName());
+
+        formatter.setDescPadding(3);
+        assertEquals("desc padding", 3, formatter.getDescPadding());
+
+        formatter.setLeftPadding(7);
+        assertEquals("left padding", 7, formatter.getLeftPadding());
+
+        formatter.setLongOptPrefix("~~");
+        assertEquals("long opt prefix", "~~", formatter.getLongOptPrefix());
+
+        formatter.setNewLine("\n");
+        assertEquals("new line", "\n", formatter.getNewLine());
+
+        formatter.setOptPrefix("~");
+        assertEquals("opt prefix", "~", formatter.getOptPrefix());
+
+        formatter.setSyntaxPrefix("-> ");
+        assertEquals("syntax prefix", "-> ", formatter.getSyntaxPrefix());
+
+        formatter.setWidth(80);
+        assertEquals("width", 80, formatter.getWidth());
+    }
+
 }
