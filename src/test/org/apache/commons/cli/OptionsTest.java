5f8a5705f94be11bbaaf6f6845d11814b02400c2
Emmanuel Bourg <ebourg@apache.org>
2008-05-30 09:11:57 +0000

    Added a test for Options.toString()
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/branches/cli-1.x@661607 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/OptionsTest.java
+++ src/test/org/apache/commons/cli/OptionsTest.java
@@ -97,4 +97,15 @@ public class OptionsTest extends TestCase
         }
     }
 
+    public void testToString() {
+        Options options = new Options();
+        options.addOption("f", "foo", true, "Foo");
+        options.addOption("b", "bar", false, "Bar");
+
+        String s = options.toString();
+        assertNotNull("null string returned", s);
+        assertTrue("foo option missing", s.toLowerCase().indexOf("foo") != -1);
+        assertTrue("bar option missing", s.toLowerCase().indexOf("bar") != -1);
+    }
+
 }
