ead1f16f5983e080e4abc98b9b5aba288f2c320f
Emmanuel Bourg <ebourg@apache.org>
2008-05-30 11:47:29 +0000

    Test coverage for OptionGroup.toString()
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/branches/cli-1.x@661665 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/OptionGroupTest.java
+++ src/test/org/apache/commons/cli/OptionGroupTest.java
@@ -197,4 +197,19 @@ public class OptionGroupTest extends TestCase
         assertTrue("Confirm --import is set", cl2.hasOption("import"));
     }
 
+    public void testToString()
+    {
+        OptionGroup group1 = new OptionGroup();
+        group1.addOption(new Option(null, "foo", false, "Foo"));
+        group1.addOption(new Option(null, "bar", false, "Bar"));
+
+        assertEquals("[--foo Foo, --bar Bar]", group1.toString());
+
+        OptionGroup group2 = new OptionGroup();
+        group2.addOption(new Option("f", "foo", false, "Foo"));
+        group2.addOption(new Option("b", "bar", false, "Bar"));
+
+        assertEquals("[-f Foo, -b Bar]", group2.toString());
+    }
+
 }
