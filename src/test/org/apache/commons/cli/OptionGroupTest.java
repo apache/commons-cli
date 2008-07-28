535beb125f0627c5399fc365208d3be24da81048
Emmanuel Bourg <ebourg@apache.org>
2008-07-28 10:25:09 +0000

    Improved the test coverage for OptionGroup and Options
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/branches/cli-1.x@680311 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/cli/OptionGroupTest.java
+++ src/test/org/apache/commons/cli/OptionGroupTest.java
@@ -210,4 +210,15 @@ public class OptionGroupTest extends TestCase
         assertEquals("[-f Foo, -b Bar]", group2.toString());
     }
 
+    public void testGetNames()
+    {
+        OptionGroup group = new OptionGroup();
+        group.addOption(OptionBuilder.create('a'));
+        group.addOption(OptionBuilder.create('b'));
+
+        assertNotNull("null names", group.getNames());
+        assertEquals(2, group.getNames().size());
+        assertTrue(group.getNames().contains("a"));
+        assertTrue(group.getNames().contains("b"));
+    }
 }
--- src/test/org/apache/commons/cli/OptionsTest.java
+++ src/test/org/apache/commons/cli/OptionsTest.java
@@ -141,4 +141,22 @@ public class OptionsTest extends TestCase
         assertTrue("bar option missing", s.toLowerCase().indexOf("bar") != -1);
     }
 
+    public void testGetOptionsGroups()
+    {
+        Options options = new Options();
+
+        OptionGroup group1 = new OptionGroup();
+        group1.addOption(OptionBuilder.create('a'));
+        group1.addOption(OptionBuilder.create('b'));
+
+        OptionGroup group2 = new OptionGroup();
+        group2.addOption(OptionBuilder.create('x'));
+        group2.addOption(OptionBuilder.create('y'));
+
+        options.addOptionGroup(group1);
+        options.addOptionGroup(group2);
+
+        assertNotNull(options.getOptionGroups());
+        assertEquals(2, options.getOptionGroups().size());
+    }
 }
