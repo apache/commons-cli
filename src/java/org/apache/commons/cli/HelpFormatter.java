6a585453d385449dc23d90479488f92f02cd6b83
Henri Yandell <bayard@apache.org>
2009-02-27 09:38:10 +0000

    Adjusting - best is to switch to an indent of 1 than do silly things like having every line have 1 character. CLI-162
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/branches/cli-1.x@748463 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/cli/HelpFormatter.java
+++ src/java/org/apache/commons/cli/HelpFormatter.java
@@ -822,7 +822,7 @@ public class HelpFormatter
         if (nextLineTabStop >= width)
         {
             // stops infinite loop happening
-            nextLineTabStop = width - 1;
+            nextLineTabStop = 1;
         }
 
         // all following lines must be padded with nextLineTabStop space 
--- src/test/org/apache/commons/cli/bug/BugCLI162Test.java
+++ src/test/org/apache/commons/cli/bug/BugCLI162Test.java
@@ -267,34 +267,15 @@ public class BugCLI162Test extends TestCase {
         HelpFormatter formatter = new HelpFormatter();
         StringWriter sw = new StringWriter();
         formatter.printHelp(new PrintWriter(sw), 22, this.getClass().getName(), "Header", options, 0, 5, "Footer");
+        System.err.println(sw.toString());
         String expected = "usage:\n" +
                           "       org.apache.comm\n" +
                           "       ons.cli.bug.Bug\n" +
                           "       CLI162Test\n" +
                           "Header\n" +
                           "-x,--extralongarg\n" +
-                          "                     T\n" +
-                          "                     h\n" +
-                          "                     i\n" +
-                          "                     s\n" +
-                          "                     d\n" +
-                          "                     e\n" +
-                          "                     s\n" +
-                          "                     c\n" +
-                          "                     r\n" +
-                          "                     i\n" +
-                          "                     p\n" +
-                          "                     t\n" +
-                          "                     i\n" +
-                          "                     o\n" +
-                          "                     n\n" +
-                          "                     i\n" +
-                          "                     s\n" +
-                          "                     L\n" +
-                          "                     o\n" +
-                          "                     n\n" +
-                          "                     g\n" +
-                          "                     .\n" +
+                          " This description is\n" +
+                          " Long.\n" +
                           "Footer\n";
         assertEquals( "Long arguments did not split as expected", expected, sw.toString() );
     }
