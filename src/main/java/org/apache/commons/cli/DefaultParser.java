1bf9e6c551b6a2e7d37291673a1ff77c338ce131
Benedikt Ritter <britter@apache.org>
2016-09-07 20:19:32 +0000

    CLI-265: Optional argument picking up next regular option as its argument. Thank you to Lynn Henderson, Martin Sandiford and Veit Guna for providing reproductions.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/trunk@1759695 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/cli/DefaultParser.java
+++ src/main/java/org/apache/commons/cli/DefaultParser.java
@@ -299,7 +299,15 @@ public class DefaultParser implements CommandLineParser
     private boolean isShortOption(String token)
     {
         // short options (-S, -SV, -S=V, -SV1=V2, -S1S2)
-        return token.startsWith("-") && token.length() >= 2 && options.hasShortOption(token.substring(1, 2));
+        if (!token.startsWith("-") || token.length() == 1)
+        {
+            return false;
+        }
+
+        // remove leading "-" and "=value"
+        int pos = token.indexOf("=");
+        String optName = pos == -1 ? token.substring(1) : token.substring(1, pos);
+        return options.hasShortOption(optName);
     }
 
     /**
