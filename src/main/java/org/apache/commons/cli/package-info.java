73d649ba3fff756dda3f98a9a987725d0b9e3499
Benedikt Ritter <britter@apache.org>
2017-03-13 10:51:43 +0000

    Remove hard coded version from package JavaDoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/cli/trunk@1786656 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/cli/package-info.java
+++ src/main/java/org/apache/commons/cli/package-info.java
@@ -16,8 +16,8 @@
  */
 
 /**
- * Commons CLI 1.3
- * 
+ * Apache Commons CLI provides a simple API for presenting, processing and validating a command line interface.
+ *
  * @version $Id$
  */
 package org.apache.commons.cli;
