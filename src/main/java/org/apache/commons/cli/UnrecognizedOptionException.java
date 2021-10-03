dc9af47ef6149c7895ffcac9ede4d33fb37dfd3b
Gary Gregory <garydgregory@gmail.com>
2021-10-03 17:28:30 -0400

    Use final.

--- src/main/java/org/apache/commons/cli/UnrecognizedOptionException.java
+++ src/main/java/org/apache/commons/cli/UnrecognizedOptionException.java
@@ -18,40 +18,41 @@
 package org.apache.commons.cli;
 
 /**
- * Exception thrown during parsing signalling an unrecognized option was seen.
+ * Thrown during parsing signaling an unrecognized option.
  */
 public class UnrecognizedOptionException extends ParseException {
+
     /**
      * This exception {@code serialVersionUID}.
      */
     private static final long serialVersionUID = -252504690284625623L;
 
-    /** The unrecognized option */
-    private String option;
+    /** The unrecognized option. */
+    private final String option;
 
     /**
-     * Construct a new <code>UnrecognizedArgumentException</code> with the specified detail message.
+     * Constructs a new <code>UnrecognizedArgumentException</code> with the specified detail message.
      *
      * @param message the detail message
      */
     public UnrecognizedOptionException(final String message) {
-        super(message);
+        this(message, null);
     }
 
     /**
-     * Construct a new <code>UnrecognizedArgumentException</code> with the specified option and detail message.
+     * Constructs a new <code>UnrecognizedArgumentException</code> with the specified option and detail message.
      *
      * @param message the detail message
      * @param option the unrecognized option
      * @since 1.2
      */
     public UnrecognizedOptionException(final String message, final String option) {
-        this(message);
+        super(message);
         this.option = option;
     }
 
     /**
-     * Returns the unrecognized option.
+     * Gets the unrecognized option.
      *
      * @return the related option
      * @since 1.2
