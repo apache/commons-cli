19fb139a8b7ca101eceb442789d68f686e94c3d2
Claude Warren <claude.warren@aiven.io>
2024-02-14 16:30:07 +0100

    CLI-322: Add '-' as an option char and implemented extensive tests (#217)
    * Added '-' as an option char and implemented extensive tests
    
    * fixed checkstyle issue
    
    * renamed 'ary' parameter, added valid character documentation
    
    * renamed 'ary' parameter, added valid character documentation
    
    * fixed issues with extra spaces

--- src/main/java/org/apache/commons/cli/OptionValidator.java
+++ src/main/java/org/apache/commons/cli/OptionValidator.java
@@ -23,24 +23,67 @@ package org.apache.commons.cli;
  * @since 1.1
  */
 final class OptionValidator {
+    /* package private for testing access */
+    /** The array of additional characters allowed as the first character in the option but not in the rest of the option */
+    static final char[] ADDITIONAL_OPTION_CHARS = {'?', '@'};
+    /** The array of additional characters allowed in the rest of the option but not in the first position */
+    static final char[] ADDITIONAL_LONG_CHARS = {'-'};
+
+    /**
+     * Checks the char array for a matching char.
+     * @param chars the char array to search
+     * @param c the char to look for.
+     * @return {@code true} if {@code c} was in {@code ary}, {@code false} otherwise.
+     */
+    private static boolean search(final char[] chars, final char c) {
+        for (char a : chars) {
+            if (a == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * Returns whether the specified character is a valid character.
-     *
+     * A character is valid if any of the following conditions are true:
+     * <ul>
+     * <li>it is a letter</li>
+     * <li>it is a currency symbol (such as '$')</li>
+     * <li>it is a connecting punctuation character (such as '_')</li>
+     * <li>it is a digit</li>
+     * <li>it is a numeric letter (such as a Roman numeral character)</li>
+     * <li>it is a combining mark</li>
+     * <li>it is a non-spacing mark</li>
+     * <li>isIdentifierIgnorable returns true for the character</li>
+     * <li>it is a hyphen/dash ('-')</li>
+     * </ul>
      * @param c the character to validate
-     * @return true if {@code c} is a letter.
+     * @return true if {@code c} is a valid character letter.
      */
     private static boolean isValidChar(final char c) {
-        return Character.isJavaIdentifierPart(c);
+        return Character.isJavaIdentifierPart(c) || search(ADDITIONAL_LONG_CHARS, c);
     }
 
     /**
      * Returns whether the specified character is a valid Option.
-     *
+     * A character is valid if any of the following conditions are true:
+     * <ul>
+     * <li>it is a letter</li>
+     * <li>it is a currency symbol (such as '$')</li>
+     * <li>it is a connecting punctuation character (such as '_')</li>
+     * <li>it is a digit</li>
+     * <li>it is a numeric letter (such as a Roman numeral character)</li>
+     * <li>it is a combining mark</li>
+     * <li>it is a non-spacing mark</li>
+     * <li>isIdentifierIgnorable returns true for the character</li>
+     * <li>it is a question mark or 'at' sign ('?' or '@')</li>
+     * </ul>
      * @param c the option to validate
      * @return true if {@code c} is a letter, '?' or '@', otherwise false.
      */
     private static boolean isValidOpt(final char c) {
-        return isValidChar(c) || c == '?' || c == '@';
+        return Character.isJavaIdentifierPart(c) || search(ADDITIONAL_OPTION_CHARS, c);
     }
 
     /**
@@ -49,9 +92,22 @@ final class OptionValidator {
      *
      * <ul>
      * <li>a single character {@code opt} that is either ' '(special case), '?', '@' or a letter</li>
-     * <li>a multi character {@code opt} that only contains letters.</li>
+     * <li>a multi character {@code opt} that only contains valid characters</li>
      * </ul>
-     * <p>
+     * </p><p>
+     * A character is valid if any of the following conditions are true:
+     * <ul>
+     * <li>it is a letter</li>
+     * <li>it is a currency symbol (such as '$')</li>
+     * <li>it is a connecting punctuation character (such as '_')</li>
+     * <li>it is a digit</li>
+     * <li>it is a numeric letter (such as a Roman numeral character)</li>
+     * <li>it is a combining mark</li>
+     * <li>it is a non-spacing mark</li>
+     * <li>isIdentifierIgnorable returns true for the character</li>
+     * <li>it is a hyphen/dash ('-')</li>
+     * </ul>
+     * </p><p>
      * In case {@code opt} is {@code null} no further validation is performed.
      *
      * @param option The option string to validate, may be null
@@ -63,18 +119,16 @@ final class OptionValidator {
             return null;
         }
 
-        // handle the single character opt
-        if (option.length() == 1) {
-            final char ch = option.charAt(0);
+        char[] chars = option.toCharArray();
 
-            if (!isValidOpt(ch)) {
-                throw new IllegalArgumentException("Illegal option name '" + ch + "'");
-            }
-        } else {
-            // handle the multi character opt
-            for (final char ch : option.toCharArray()) {
-                if (!isValidChar(ch)) {
-                    throw new IllegalArgumentException("The option '" + option + "' contains an illegal " + "character : '" + ch + "'");
+        if (!isValidOpt(chars[0])) {
+            throw new IllegalArgumentException("Illegal option name '" + chars[0] + "'");
+        }
+        // handle the multi-character opt
+        if (option.length() > 1) {
+            for (int i = 1; i < chars.length; i++) {
+                if (!isValidChar(chars[i])) {
+                    throw new IllegalArgumentException("The option '" + option + "' contains an illegal " + "character : '" + chars[i] + "'");
                 }
             }
         }
