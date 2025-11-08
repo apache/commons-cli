b65ef1c9957acd8d38273142b25fe20d8351acab
Gary Gregory <garydgregory@gmail.com>
2025-11-08 11:40:14 -0500

    Remove dead comment

--- src/main/java/org/apache/commons/cli/CommandLineParser.java
+++ src/main/java/org/apache/commons/cli/CommandLineParser.java
@@ -34,21 +34,6 @@ public interface CommandLineParser {
     CommandLine parse(Options options, String[] arguments) throws ParseException;
 
     /**
-     * Parses the arguments according to the specified options and properties.
-     *
-     * @param options the specified Options.
-     * @param arguments the command line arguments.
-     * @param properties command line option name-value pairs.
-     * @return the list of atomic option and value tokens.
-     * @throws ParseException if there are any problems encountered while parsing the command line tokens.
-     */
-    /*
-     * To maintain binary compatibility, this is commented out. It is still in the abstract Parser class, so most users will
-     * still reap the benefit. CommandLine parse(Options options, String[] arguments, Properties properties) throws
-     * ParseException;
-     */
-
-    /**
      * Parses the arguments according to the specified options.
      *
      * @param options the specified Options.
