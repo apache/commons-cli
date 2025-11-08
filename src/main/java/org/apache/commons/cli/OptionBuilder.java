395346c58f510fcff79893d0adeb9961ab430a43
Gary Gregory <garydgregory@gmail.com>
2025-11-08 15:05:52 -0500

    Better exception message

--- src/main/java/org/apache/commons/cli/OptionBuilder.java
+++ src/main/java/org/apache/commons/cli/OptionBuilder.java
@@ -72,7 +72,7 @@ public final class OptionBuilder {
     public static Option create() throws IllegalArgumentException {
         if (longOption == null) {
             reset();
-            throw new IllegalStateException("must specify longopt");
+            throw new IllegalStateException("longopt missing");
         }
         return create(null);
     }
