3afb52e7f1b3a04e0a1e531ce99e9718ed5bc901
Luis Villa <luis@lu.is>
2026-01-13 11:56:45 +0000

    Fix SonarQube issue java:S1133: Do not forget to remove this deprecated code somed

--- src/main/java/org/apache/commons/cli/HelpFormatter.java
+++ src/main/java/org/apache/commons/cli/HelpFormatter.java
@@ -259,6 +259,7 @@ public class HelpFormatter {
      * @deprecated Scope will be made private for next major version - use get/setLeftPadding methods instead.
      */
     @Deprecated
+    @SuppressWarnings("java:S1133") // Deprecated code is kept for backward compatibility
     public int defaultLeftPad = DEFAULT_LEFT_PAD;
 
     /**
